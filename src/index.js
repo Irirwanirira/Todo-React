d14b4d8385887e09a183c3610a777a7d8707c940
Irirwanirira <irijoseph41@gmail.com>
2022-11-09 10:39:38 +0200

    set linters

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import TodoContaniner from './TodoComponents/TodoContainer';
-import "./Apps.css"
+import './Apps.css';
+import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    
-    <TodoContaniner />
+    <App />
   </React.StrictMode>
 );
-
-
